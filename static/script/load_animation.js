ed031f2e8778fcced72a8709b69d5159e30abf95
Jesvi Jonathan <47633894+jesvijonathan@users.noreply.github.com>
2022-01-22 19:41:02 +0530

    made scrollable during load

--- static/script/load_animation.js
+++ static/script/load_animation.js
@@ -5,7 +5,9 @@ console.log(loading_var);
 document.onreadystatechange = function () {
   if (document.readyState == "complete") {
     document.querySelector("#load-fill").style.visibility = "hidden";
+    
     document.querySelector("body").style.overflowY = "scroll";
+    
     //document.querySelector("#cover-name").style.position = "relative";
     document.querySelector(".loading").style.visibility = "hidden";
     document.querySelector(".loading").style.position = "absolute";
@@ -15,7 +17,10 @@ document.onreadystatechange = function () {
     // myDiv.scrollTop = 0;
   } else {
     document.querySelector("#load-fill").style.visibility = "visible";
-    document.querySelector("body").style.overflowY = "hidden";
+    
+    //document.querySelector("body").style.overflowY = "hidden";
+    document.querySelector("body").style.overflowY = "scroll";
+    
     //document.querySelector("#cover-name").style.position = "fixed";
     document.querySelector(".loading").style.visibility = "visible";
     document.querySelector(".loading").style.position = "fixed";
